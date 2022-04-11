706a64343881697f3620c9a0ecc81c76d5ed894a
Zachary Arsenault <zaki55515@hotmail.ca>
2022-04-10 23:24:19 -0400

    Mettre tout t les icone

--- src/composants/Tache.jsx
+++ src/composants/Tache.jsx
@@ -1,14 +1,22 @@
 import './Tache.scss';
 import { formaterDate } from '../code/utilitaire';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import DoDisturbOnIcon from '@mui/icons-material/DoDisturbOn';
+import IconButton from '@mui/material/IconButton';
 
 
 export default function Tache({id, texte, date}) {
   return (
     <div className="Tache">
-      Basculer
+       <IconButton color='success'>
+        <CheckCircleIcon/>
+      </IconButton>
+      
       <span className="texte">{texte}</span>
       <span className="date">{formaterDate(date.seconds)}</span>
-      Supprimer
+      <IconButton color='error'>
+        <DoDisturbOnIcon/>
+      </IconButton>
     </div>
   );
 }
